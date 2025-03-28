2600d83f80505c908e571fc72dcfa066d983660c
venustuspl <venustus.pl@gmail.com>
2025-03-28 18:58:15 +0100

    rejest-klientów

---
+++ src/app/modules/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../shared/material/material.module';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [CommonModule, MaterialModule, RouterModule],
+  exports: [HeaderComponent],
+})
+export class CoreModule {}
