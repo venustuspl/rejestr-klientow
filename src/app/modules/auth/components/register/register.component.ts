162707e74b58f3d4036d007aaeafd70f31b7f20f
venustuspl <venustus.pl@gmail.com>
2025-04-18 20:02:34 +0200

    rejest-klientów - register

--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  hide = true;
+  registerForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.email,
+      Validators.minLength(5),
+      Validators.maxLength(50),
+    ]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', { validators: [Validators.required] }),
+  });
 
+  get controls() {
+    return this.registerForm.controls;
+  }
+
+  ngOnInit(): void {
+    // this.registerForm.controls.email.valueChanges.subscribe((text) => {
+    //   console.log(text);
+    // });
+    console.log('');
+    // this.registerForm.controls.email.disable();
+    // this.controls.username.setValidators();
+    // this.controls.username.addValidators();
+    // this.registerForm.patchValue();
+    // this.registerForm.setValue();
+  }
+
+  getErrorMessage(control: FormControl) {
+    if (control.hasError('required')) {
+      return 'Musisz wpisać jakąś wartość.';
+    }
+
+    if (control.hasError('minlength')) {
+      return 'Przekazałeś za mało znaków w kontrolce.';
+    }
+
+    if (control.hasError('maxlength')) {
+      return 'Przekazałeś za dużo znaków w kontrolce.';
+    }
+
+    return control.hasError('email') ? 'Nieprawidłowy adres e-mail' : '';
+  }
+
+  onRegister() {
+    console.log(this.registerForm.value);
+    // console.log(this.registerForm.getRawValue());
+  }
 }
--- src/app/modules/core/components/header/header.component.ts
+++ src/app/modules/core/components/header/header.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/user.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  user: User | null = null;
+  sub!: Subscription;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.sub = this.authService.user.subscribe({
+      next: (value) => {
+        this.user = value;
+      },
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
